b1447d910b88ac04fafc370a808805b8814c55f1
Saular Raffi <saularraffi@yahoo.com>
2021-07-20 12:10:09 -0400

    commit

--- server/scripts/docker_build.sh
+++ server/scripts/docker_build.sh
@@ -4,7 +4,7 @@
 repo="https://github.com/saularraffi/test-app.git"
 branch="main"
 app_name="test-app"
-pwnpals_apps_dir="/tmp/pwn_pals_apps"
+pwnpals_apps_dir="~/tmp/pwnpals"
 app_dir="$pwnpals_apps_dir"/"$app_name"
 
 # initializing dirctories
@@ -21,6 +21,21 @@ else
 fi
 
 # build docker image and run container
+echo docker: sudo docker build -t "$app_name" "$app_dir"
 sudo docker build -t "$app_name" "$app_dir"
+[ $? == 0 ] && echo [+] docker image built successfully
+[ $? == 1 ] && echo [-] failed to build docker image && exit 1
+
 app_port=$(cat $app_dir/Dockerfile  | grep EXPOSE | awk -F ' ' '{print $2}')
+echo docker: sudo docker run -dp 8080:$app_port --network host "$app_name"
 sudo docker run -dp 8080:$app_port --network host "$app_name"
+[ $? == 0 ] && echo [+] docker contianer ran successfully
+[ $? == 1 ] && echo [-] failed to run docker container && exit 1
+
+container_id=$(sudo docker ps | grep $app_name | awk -F ' ' '{print $1}')
+echo docker: sudo docker stop $container_id
+sudo docker stop $container_id
+
+rm -rf "$app_dir"
+
+exit 0

7cbe5f068b488c73d1714f1207438d04df3538ab
Saular Raffi <saular.raffi@yahoo.com>
2021-11-13 17:32:32 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import BuildImageForm from './components/BuildImageForm'
+import AppList from './components/AppList'
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Pwn Pals</h1>
+      <BuildImageForm />
+      <AppList />
     </div>
   );
 }

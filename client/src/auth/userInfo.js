d3ec7a1074fc44c9952fd08f087cceecbcd6ec67
Saular <saular.raffi@yahoo.com>
2022-01-23 15:37:10 -0500

    commit

--- client/src/auth/userInfo.js
+++ client/src/auth/userInfo.js
@@ -6,9 +6,6 @@ export function isLoggedIn() {
     if (cookies.get('connect.sid')) {
         return true
     }
-    // if (localStorage.getItem('loggedIn') === 'true') {
-    //     return true
-    // }
     else {
         return false
     }
@@ -29,17 +26,3 @@ export function getUserId() {
 export function setUserId(userId) {
     localStorage.setItem('userId', userId)
 }
-
-// export function setLoggedInStatus(status) {
-//     localStorage.setItem('loggedIn', status)
-// }
-
-// export function logOut() {
-//     axios.post(`${process.env.REACT_APP_BACKEND}/api/auth/logout`, {}, { withCredentials: true })
-//     .then(res => {
-//         localStorage.removeItem('username')
-//         localStorage.removeItem('userId')
-//         navigate('/')
-//     })
-//     .catch(err => console.log(err))
-// }
